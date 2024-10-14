fef9bf540b16668b7b70066c6423cd2adf287408
ifox6677 <zhangjq0908@outlook.com>
2024-10-14 18:15:12 +0800

    Update replace_files.sh

--- scripts/replace_files.sh
+++ scripts/replace_files.sh
@@ -17,7 +17,8 @@ declare -A file_map=(
   ["trunk/user/ttyd"]="trunk/user/ttyd"
   ["trunk/user/trojan"]="trunk/user/trojan"
   ["trunk/libs/boost"]="trunk/libs/boost"  
-  ["trunk/libs/libwebsockets"]="trunk/libs/libwebsockets"	
+  ["trunk/libs/libwebsockets"]="trunk/libs/libwebsockets"
+  ["trunk/libs/c-ares"]="trunk/libs/c-ares"  
   ["trunk/libs/libjson-c"]="trunk/libs/libjson-c"  
   # 根据需要添加更多文件或目录映射
 )
